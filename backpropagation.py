c434f2a293abd1ffae83b8d28acbf7dfe3df235c
snuf600 <arnaudhallemans@hotmail.com>
2020-10-01 13:01:11 +0200

    Met biases

--- backpropagation.py
+++ backpropagation.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-x = np.random.rand(4,1)
+x = np.random.rand(3,1)
 learning_rate = 0.01
 
 class layer:
@@ -19,16 +19,20 @@ class layer:
 
     #dz/dw = a_L-1 = x
 
-    def backProp(self, x, z, c_func):#voor de laatste layer is c_func d_cost en daarvoor da
+    def backPropw(self, x, z, c_func):
         dw = np.dot(x, (self.d_softmax(z) * c_func).T)
         self.w -= learning_rate * dw
+
+    def backPropb(self, z, c_func):
+        db = (self.d_softmax(z) * c_func)
+        self.b -= learning_rate * db.T
 
     def calc_da(self, w, z, a, y):
         self.da = np.dot(w, self.d_softmax(z) * d_cost(a,y))
 
-    def backProp2(self, x, z, da): #da is dC/da en komt overeen met da van de volgende layer
-        dw = np.dot(x, (self.d_softmax(z) * da).T)
-        self.w -= learning_rate * dw
+    # def backProp2(self, x, z, da): #da is dC/da en komt overeen met da van de volgende layer
+    #     dw = np.dot(x, (self.d_softmax(z) * da).T)
+    #     self.w -= learning_rate * dw
 
 def d_cost( a_L,y): #dC/da
     return 2*(a_L-y)
@@ -39,26 +43,31 @@ def cost(a_L,y):
 
 
 
-
-
-y = np.random.rand(3,1)
-l1 = layer(4,4)
-l1.forward(x)
-l1.a = l1.softmax(l1.z)
-l2 = layer(4,3)
-l2.forward(l1.a)
-l2.a = l2.softmax(l2.z)
-
-print ("cost is", cost(l2.a,y))
-for i in range(100000):
-    l2.backProp(l1.a,l2.z,d_cost(l2.a,y))
-    l2.calc_da(l2.w, l2.z, l2.a,y)
-    l1.backProp(x, l1.z, l2.da)
+costdiff = []
+for j in range(10):
+    y = np.random.rand(3,1)
+    l1 = layer(3,10)
     l1.forward(x)
     l1.a = l1.softmax(l1.z)
+    l2 = layer(10,3)
     l2.forward(l1.a)
     l2.a = l2.softmax(l2.z)
-print ("cost is",cost(l2.a,y))
+
+    cost1 =  cost(l2.a,y)
+    for i in range(10000):
+        l2.backPropw( l1.a,l2.z,d_cost(l2.a,y))
+        l2.backPropb( l2.z,d_cost(l2.a,y))
+        l2.calc_da(l2.w, l2.z, l2.a,y)
+        l1.backPropw( x, l1.z, l2.da)
+        l1.backPropb( l1.z, l2.da)
+        l1.forward(x)
+        l1.a = l1.softmax(l1.z)
+        l2.forward(l1.a)
+        l2.a = l2.softmax(l2.z)
+    cost2 = cost(l2.a,y)
+    costdiff.append((cost1-cost2)/cost1)
+    print(j)
+print (np.mean(costdiff), costdiff)
 
 # print(l1.w)
 # print("cost is", cost(l1.a,y))
